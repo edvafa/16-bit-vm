1d585ee659c4cf286cb37dde90c6d0e983e196fc
EduVF <eduardvf@outlook.com>
2023-09-09 21:56:24 +0200

    add more instructions

--- cpu.c
+++ cpu.c
@@ -92,6 +92,11 @@ void execute(CPU *cpu, uint8_t ins)
 {
     switch (ins)
     {
+
+        ////////////////////////////////
+        // MOVING
+        ////////////////////////////////
+
     case MOV_LIT_REG:
     {
         const uint16_t lit = fetch16(cpu);
@@ -124,13 +129,183 @@ void execute(CPU *cpu, uint8_t ins)
         cpu->reg[reg] = get_uint16(cpu, address);
         return;
     }
+    case MOV_LIT_MEM:
+    {
+        const uint16_t val = fetch16(cpu);
+        const uint16_t address = fetch16(cpu);
+        set_uint16(cpu, address, val);
+        return;
+    }
+    case MOV_REG_PTR_REG:
+    {
+        const uint8_t reg_with_p = fetch_reg(cpu);
+        const uint8_t reg_to_wr = fetch_reg(cpu);
+        const uint16_t p = cpu->reg[reg_with_p];
+        const uint16_t val = get_uint16(cpu, p);
+        cpu->reg[reg_to_wr] = val;
+        return;
+    }
+    case MOV_LIT_OFF_REG:
+    {
+        const uint16_t base_address = fetch16(cpu);
+        const uint8_t reg_with_off = fetch_reg(cpu);
+        const uint8_t reg_to_wr = fetch_reg(cpu);
+        const uint16_t offset = cpu->reg[reg_with_off];
+
+        const uint16_t val = get_uint16(cpu, base_address + offset);
+        cpu->reg[reg_to_wr] = val;
+        return;
+    }
+
+        ////////////////////////////////
+        // ARITHMETIC
+        ////////////////////////////////
+
     case ADD_REG_REG:
     {
-        const uint16_t v0 = cpu->reg[fetch(cpu)];
-        const uint16_t v1 = cpu->reg[fetch(cpu)];
+        const uint16_t v0 = cpu->reg[fetch_reg(cpu)];
+        const uint16_t v1 = cpu->reg[fetch_reg(cpu)];
         cpu->reg[ac] = v0 + v1;
         return;
     }
+    case ADD_LIT_REG:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = val + lit;
+        return;
+    }
+    case SUB_LIT_REG:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = val - lit;
+        return;
+    }
+    case SUB_REG_LIT:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = lit - val;
+        return;
+    }
+    case SUB_REG_REG:
+    {
+        const uint8_t v0 = cpu->reg[fetch_reg(cpu)];
+        const uint8_t v1 = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = v0 - v1;
+        return;
+    }
+    case MUL_LIT_REG:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = val * lit;
+        return;
+    }
+    case MUL_REG_REG:
+    {
+        const uint16_t v0 = cpu->reg[fetch_reg(cpu)];
+        const uint16_t v1 = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = v0 * v1;
+        return;
+    }
+    case INC_REG:
+    {
+        cpu->reg[fetch_reg(cpu)]++;
+        return;
+    }
+    case DEC_REG:
+    {
+        cpu->reg[fetch_reg(cpu)]--;
+        return;
+    }
+
+        ////////////////////////////////
+        // LOGICAL
+        ////////////////////////////////
+
+    case LSF_REG_LIT:
+    {
+        const uint8_t reg = fetch_reg(cpu);
+        const uint8_t lit = fetch(cpu);
+        cpu->reg[reg] <<= lit;
+        return;
+    }
+    case LSF_REG_REG:
+    {
+        const uint8_t reg_to_shift = fetch_reg(cpu);
+        const uint8_t reg_with_val = fetch_reg(cpu);
+        cpu->reg[reg_to_shift] <<= cpu->reg[reg_with_val];
+        return;
+    }
+    case RSF_REG_LIT:
+    {
+        const uint8_t reg = fetch_reg(cpu);
+        const uint8_t lit = fetch(cpu);
+        cpu->reg[reg] >>= lit;
+        return;
+    }
+    case RSF_REG_REG:
+    {
+        const uint8_t reg_to_shift = fetch_reg(cpu);
+        const uint8_t reg_with_val = fetch_reg(cpu);
+        cpu->reg[reg_to_shift] >>= cpu->reg[reg_with_val];
+        return;
+    }
+    case AND_REG_LIT:
+    {
+        const uint8_t reg = fetch_reg(cpu);
+        const uint16_t lit = fetch16(cpu);
+        cpu->reg[ac] = cpu->reg[reg] & lit;
+        return;
+    }
+    case AND_REG_REG:
+    {
+        const uint16_t v0 = cpu->reg[fetch_reg(cpu)];
+        const uint16_t v1 = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = v0 & v1;
+        return;
+    }
+    case OR_REG_LIT:
+    {
+        const uint8_t reg = fetch_reg(cpu);
+        const uint16_t lit = fetch16(cpu);
+        cpu->reg[ac] = cpu->reg[reg] | lit;
+        return;
+    }
+    case OR_REG_REG:
+    {
+        const uint16_t v0 = cpu->reg[fetch_reg(cpu)];
+        const uint16_t v1 = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = v0 | v1;
+        return;
+    }
+    case XOR_REG_LIT:
+    {
+        const uint8_t reg = fetch_reg(cpu);
+        const uint16_t lit = fetch16(cpu);
+        cpu->reg[ac] = cpu->reg[reg] ^ lit;
+        return;
+    }
+    case XOR_REG_REG:
+    {
+        const uint16_t v0 = cpu->reg[fetch_reg(cpu)];
+        const uint16_t v1 = cpu->reg[fetch_reg(cpu)];
+        cpu->reg[ac] = v0 ^ v1;
+        return;
+    }
+    case NOT:
+    {
+        const uint8_t reg = fetch_reg(cpu);
+        cpu->reg[reg] = ~cpu->reg[reg];
+        return;
+    }
+
+        ////////////////////////////////
+        // BRANCHING
+        ////////////////////////////////
+
     case JMP_NOT_EQ:
     {
         const uint16_t lit = fetch16(cpu);
@@ -139,6 +314,99 @@ void execute(CPU *cpu, uint8_t ins)
             cpu->reg[ip] = address;
         return;
     }
+    case JNE_REG:
+    {
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        const uint16_t address = fetch16(cpu);
+        if (val != cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JEQ_LIT:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t address = fetch16(cpu);
+        if (lit == cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JEQ_REG:
+    {
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        const uint16_t address = fetch16(cpu);
+        if (val == cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JLT_LIT:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t address = fetch16(cpu);
+        if (lit < cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JLT_REG:
+    {
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        const uint16_t address = fetch16(cpu);
+        if (val < cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JGT_LIT:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t address = fetch16(cpu);
+        if (lit > cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JGT_REG:
+    {
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        const uint16_t address = fetch16(cpu);
+        if (val > cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JLE_LIT:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t address = fetch16(cpu);
+        if (lit <= cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JLE_REG:
+    {
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        const uint16_t address = fetch16(cpu);
+        if (val <= cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JGE_LIT:
+    {
+        const uint16_t lit = fetch16(cpu);
+        const uint16_t address = fetch16(cpu);
+        if (lit >= cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+    case JGE_REG:
+    {
+        const uint16_t val = cpu->reg[fetch_reg(cpu)];
+        const uint16_t address = fetch16(cpu);
+        if (val >= cpu->reg[ac])
+            cpu->reg[ip] = address;
+        return;
+    }
+
+        ////////////////////////////////
+        // OTHER
+        ////////////////////////////////
+
     case PSH_LIT:
     {
         const uint16_t val = fetch16(cpu);
